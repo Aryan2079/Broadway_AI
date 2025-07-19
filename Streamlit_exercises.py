f60a0811e5e5a69c28f838728b4c878086eb0cc1
Aryan Bhattarai <bhattaraiaryan78@gmail.com>
2025-07-19 20:06:24 +0545

    Assignment-2.0.1 | Streamlit exercises

--- Streamlit_exercises.py
+++ Streamlit_exercises.py
@@ -4,21 +4,24 @@ from sklearn.datasets import load_iris
 
 iris = load_iris()
 
+print(iris.feature_names)
 
 petal_length = st.number_input('enter petal length')
 petal_width = st.number_input('enter petal width')
-speial_length = st.number_input('enter speial length')
+sepal_length = st.number_input('enter speial length')
 sepal_width = st.number_input('enter sepal length')
 
 flower_btn = st.button('predict flower')
 
 loaded_model = joblib.load('../models/iris_classifier_knn_model.joblib')
 
-sample = [[petal_length,petal_width,speial_length,sepal_width]]
-preds = loaded_model.predict(sample)
+sample = [[sepal_length,sepal_width,petal_length,petal_width]]
+pred = loaded_model.predict(sample)[0]
+
+# st.write(f'your flower is {iris.target_names[pred]}')
 
 if flower_btn:
-    st.write(f'your flower is {str(iris.target_names[0])}')
+    st.write(f'your flower is {iris.target_names[pred]}')
 
 
 
@@ -33,7 +36,7 @@ bmi_btn =st.button('show BMI')
 
 if bmi_btn:
     bmi = weight/((height/3.28)**2)
-    
+
     if bmi<16:
         st.error('Extremely Underweight')
     elif bmi>=16 and bmi<18.5:
